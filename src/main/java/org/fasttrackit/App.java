711e6df0d8aeca26b9f5c9102c23e67f246f443f
Deac Iulian <deaciulian91@gmail.com>
2019-10-15 16:17:21 +0300

    Added overloading for methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -11,8 +11,8 @@ public class App
         Operations value = new Operations();
 
         System.out.println("The gathering is equal with  " + value.gat(9,7));
-        System.out.println("The decreasing is equal with  " + value.dec(9,7));
-        System.out.println("The multiplication is equal with  " + value.mul(9,7));
-        System.out.println("The division is equal with  " + value.div(9,7));
+        System.out.println("The decreasing is equal with  " + value.dec(7.5,6.1));
+        System.out.println("The multiplication is equal with  " + value.mul(9,7.2));
+        System.out.println("The division is equal with  " + value.div(12.5,7));
     }
 }
--- src/main/java/org/fasttrackit/Operations.java
+++ src/main/java/org/fasttrackit/Operations.java
@@ -6,13 +6,25 @@ public class Operations {
     static double gat(double a, double b) {
         return a + b;
     }
+    static int gat(int a, int b) {
+        return a + b;
+    }
     static double dec(double a, double b) {
+        return a - b;
+    }
+    static int dec(int a, int b) {
         return a - b;
     }
     static double mul(double a, double b) {
         return a * b;
     }
+    static int mul(int a, int b) {
+        return a * b;
+    }
     static double div(double a, double b) {
+        return a / b;
+    }
+    static int div(int a, int b) {
         return a / b;
     }
 
