c33a6fdaa55860cce0557d4c2a62e0cbd705daed
Deac Iulian <deaciulian91@gmail.com>
2019-10-15 14:38:01 +0300

    Created calculator methods

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -6,8 +6,13 @@ package org.fasttrackit;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+
+        Operations value = new Operations();
+
+        System.out.println("The gathering is equal with  " + value.gat(9,7));
+        System.out.println("The decreasing is equal with  " + value.dec(9,7));
+        System.out.println("The multiplication is equal with  " + value.mul(9,7));
+        System.out.println("The division is equal with  " + value.div(9,7));
     }
 }
---
+++ src/main/java/org/fasttrackit/Operations.java
@@ -0,0 +1,19 @@
+package org.fasttrackit;
+
+
+public class Operations {
+
+    static double gat(double a, double b) {
+        return a + b;
+    }
+    static double dec(double a, double b) {
+        return a - b;
+    }
+    static double mul(double a, double b) {
+        return a * b;
+    }
+    static double div(double a, double b) {
+        return a / b;
+    }
+
+}
